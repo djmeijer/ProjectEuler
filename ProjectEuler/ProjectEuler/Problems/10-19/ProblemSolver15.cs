09e92ab3da66ce8fa9dc849da5c1a3165eafd061
djmeijer <djmeijer@icloud.com>
2014-01-13 01:49:13 +0100

    Problem 15 solved

---
+++ ProjectEuler/ProjectEuler/Problems/10-19/ProblemSolver15.cs
@@ -0,0 +1,28 @@
+﻿namespace ProjectEuler
+{
+    class ProblemSolver15 : Solver
+    {
+        /* Lattice paths
+         * 
+         * Starting in the top left corner of a 2×2 grid,
+         * and only being able to move to the right and down,
+         * there are exactly 6 routes to the bottom right corner.
+         * 
+         * How many such routes are there through a 20×20 grid?
+         * 
+         */
+
+        protected override void DoCalculation()
+        {
+            const int gridSize = 20;
+            long paths = 1;
+
+            for (int i = 0; i < gridSize; i++)
+            {
+                paths *= (2 * gridSize) - i;
+                paths /= i + 1;
+            }
+            this.SetAnswer(paths);
+        }
+    }
+}
--- ProjectEuler/ProjectEuler/Program.cs
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -8,7 +8,7 @@ namespace ProjectEuler
         [STAThread]
         static void Main()
         {
-            Solver solver = new ProblemSolver14();
+            Solver solver = new ProblemSolver15();
         }
     }
 }
