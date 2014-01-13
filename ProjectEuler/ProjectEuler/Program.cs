defb6a8123408c11fb017f13125eeb71bdb8b22d
djmeijer <djmeijer@icloud.com>
2014-01-13 02:00:50 +0100

    Problem 16 solved

--- ProjectEuler/ProjectEuler/Program.cs
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -8,7 +8,7 @@ namespace ProjectEuler
         [STAThread]
         static void Main()
         {
-            Solver solver = new ProblemSolver15();
+            Solver solver = new ProblemSolver16();
         }
     }
 }
