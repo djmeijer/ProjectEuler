91d8f5f6d8c541ece162c44abaddf76f14075f7d
djmeijer <djmeijer@icloud.com>
2014-02-03 14:42:30 +0100

    Solved problem 17

--- ProjectEuler/ProjectEuler/Problems/10-19/ProblemSolver17.cs
+++ ProjectEuler/ProjectEuler/Problems/10-19/ProblemSolver17.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Linq;
+using System.Collections.Generic;
 
 namespace ProjectEuler
 {
@@ -25,7 +26,7 @@ namespace ProjectEuler
         protected override void DoCalculation()
         {
             int lettersUsed = 0;
-            for (int i = 1; i <= 40; i++)
+            for (int i = 1; i <= 1000; i++)
                 lettersUsed += this.GetNumberOfLetters(i);
             this.SetAnswer(lettersUsed);
         }
@@ -33,7 +34,6 @@ namespace ProjectEuler
         private int GetNumberOfLetters(int i)
         {
             string text = this.GetTextFromNumber(i);
-            Console.WriteLine(i + ": " + text);
             int numberOfLetters = 0;
             foreach (char c in text)
                 if (c != ' ' && c != '-')
@@ -43,52 +43,95 @@ namespace ProjectEuler
 
         private string GetTextFromNumber(int i)
         {
+            List<int> digits = this.GetDigitsList(i);
+            #region Text
             string text = "";
             string[] words = new string[] {
-                "zero",         // 0
-                "one",          // 1
-                "two",          // 2
-                "three",        // 3
-                "four",         // 4
-                "five",         // 5
-                "six",          // 6
-                "seven",        // 7
-                "eight",        // 8
-                "nine",         // 9
-                "ten",          // 10
-                "eleven",       // 11
-                "twelve",       // 12
-                "teen",         // 13
-                "thir",         // 14
-                "fif",          // 15
-                "eigh",     // 16
-                "twen",         // 17
-                "ty",           // 18
-                "eighty",       // 19
-                "hundred",      // 20
-                "thousand"      // 21
+                "one",          // 0
+                "two",          // 1
+                "three",        // 2
+                "four",         // 3
+                "five",         // 4
+                "six",          // 5
+                "seven",        // 6
+                "eight",        // 7
+                "nine",         // 8
+                "ten",          // 9
+                "eleven",       // 10
+                "twelve",       // 11
+                "teen",         // 12
+                "thir",         // 13
+                "fif",          // 14
+                "eigh",         // 15
+                "twen",         // 16
+                "ty",           // 17
+                "eighty",       // 18
+                "hundred",      // 19
+                "thousand",     // 20
+                "and",          // 21
+                "for"           // 22
             };
-            if (i == 1000)
-                text = words[1] + words[20];
-            else if (i > 99 && i < 1000)
+            #endregion
+
+            if (digits.Count == 4)
+                text = words[0] + " " + words[20];
+            else
             {
+                if (digits.Count == 3)
+                {
+                    text += words[digits[2] - 1] + " " + words[19] + " ";
+                    i -= digits[2] * 100;
+                    if (i > 0)
+                        text += words[21] + " ";
+                }
                 if (i >= 0 && i <= 19)
-                    if (i >= 0 && i <= 12)
-                        text = words[i];
+                    if (i > 0 && i <= 12)
+                        text += words[i - 1];
+                    else if (i == 0)
+                        ;
                     else
+                    {
                         if (i == 13)
-                            text = words[i + 1];
-                        else if (i == 14)
-                            text = words[4];
+                            text += words[i];
                         else if (i == 15)
-                            text = words[i];
-                        else if (i == 16 || i == 17 || i == 19)
-                            text = words[i - 10];
+                            text += words[i - 1];
+                        else if ((new int[] { 14, 16, 17, 19 }).Contains(i))
+                            text += words[i - 11];
                         else if (i == 18)
-                            text = words[16];
-                        text += words[13];
+                            text += words[i - 3];
+                        text += words[12];
+                    }
+                else
+                {
+                    if (digits[1] == 2)
+                        text += words[16] + words[17];
+                    else if (digits[1] == 3)
+                        text += words[13] + words[17];
+                    else if (digits[1] == 4)
+                        text += words[22] + words[17];
+                    else if (digits[1] == 5)
+                        text += words[14] + words[17];
+                    else if (digits[1] == 8)
+                        text += words[15] + words[17];
+                    else
+                        text += words[digits[1] - 1] + words[17];
+                    if (digits[0] > 0)
+                        text += "-" + words[digits[0] - 1];
+                }
             }
             return text;
+        }
+
+        private List<int> GetDigitsList(int i)
+        {
+            List<int> digits = new List<int>();
+            while (i >= 10)
+            {
+                digits.Add(i % 10);
+                i /= 10;
+            }
+            digits.Add(i % 10);
+            return digits;
         }
     }
 }
