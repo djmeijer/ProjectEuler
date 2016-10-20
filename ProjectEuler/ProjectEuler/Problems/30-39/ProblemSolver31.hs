1f99cb45dc9f615e87efa89287eb4e53dfebd2bc
djmeijer <djmeijer@icloud.com>
2016-10-20 22:16:00 +0200

    Ugh

--- ProjectEuler/ProjectEuler/Problems/30-39/ProblemSolver31.hs
+++ ProjectEuler/ProjectEuler/Problems/30-39/ProblemSolver31.hs
@@ -1,12 +1,13 @@
 ﻿main :: IO ()
-main = print $ payments 200 [1,2,5,10,20,50,100,200]
+main = print $ payments 200 coins (length coins)
 
-payments :: Int -> [Int] -> Int
-payments x a = sum test 0 x a
+coins :: [Int]
+coins = [1,2,5,10,20,50,100,200]
 
-test :: Int -> Int -> [Int] -> Int
-test x y a
-   | x < y     = payments 1 a
-   | x == y    = 1
-   | x > y     = 0
-   | otherwise = 0
+-- How does this work!?
+payments :: Int -> [Int] -> Int -> Int
+payments t c s
+   | t < 0     = 0
+   | t == 0    = 1
+   | s == 1    = 1
+   | otherwise = payments t c (s - 1) + payments (t - (c !! (s - 1))) c s
